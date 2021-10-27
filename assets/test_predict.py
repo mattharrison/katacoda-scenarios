1b1cb00548c6a3e44ce66c821e8e9c8397a4cf57
matt <matthewharrison@gmail.com>
2021-10-27 17:43:07 -0600

    Fix typo

--- assets/test_predict.py
+++ assets/test_predict.py
@@ -3,3 +3,11 @@ import predict
 def test_load():
     res = predict.get_data()
     assert res.shape == (2930, 82)
+
+def test_tweak_clf():
+    df = predict.get_data()
+    res = predict.tweak_ames_classification(df)
+    count_missing_numbers = res.select_dtypes('number').isna().sum().sum()
+    assert count_missing_numbers == 0, 'Should replace missing numbers with zero'
+    count_missing_objects = res.select_dtypes('object').isna().sum().sum()
+    assert count_missing_objects == 0, 'Should replace missing Electricity values'
